c926669e7dec21f3e4f806e2759e7b2a6639a1d9
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-04-25 02:37:08 -0400

    Funciones flechas primer encuentro

---
+++ FuncionesEnJavascript/funcionesFlechas.js
@@ -0,0 +1,8 @@
+/*************************************
+ * Funciones Flechas o Arrow Function
+ *************************************/
+
+const sumar = (a, b)=> a + b;
+resultado = sumar(5,8);
+console.log("La suma es :" + resultado);
+
