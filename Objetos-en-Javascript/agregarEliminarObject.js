429c13ac5fd6f779db1a66ccec88edce85d66613
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-04-29 01:27:45 -0400

    formas de imprimir un objeto en JS

--- Objetos-en-Javascript/agregarEliminarObject.js
+++ Objetos-en-Javascript/agregarEliminarObject.js
@@ -23,3 +23,23 @@ delete persona.tel;
 
 console.log(persona);
 
+// Distintas formas de imprimir un object en Js
+
+// Concatenar cada valor  de cada propiedad
+console.log(persona.nombre + ',' + persona.apellido);
+
+// for in
+
+for ( nombrePropiedad in persona){
+    console.log(persona[nombrePropiedad]);
+}
+
+// imprimiendo recorriendo el array personaArray
+
+let personaArray = Object.values(persona);
+ console.log(personaArray);
+
+// imprimiendo transformandolo en un string
+
+ let personaString = JSON.stringify(persona);
+ console.log(personaString);
