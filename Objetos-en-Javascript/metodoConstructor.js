ce2af73df2d3166f2fe272e122d8d3b4cbe2d43d
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-04-29 02:12:35 -0400

    agregar métodos a un constructor de objetos

--- Objetos-en-Javascript/metodoConstructor.js
+++ Objetos-en-Javascript/metodoConstructor.js
@@ -4,14 +4,26 @@
 
 // funcion constructor de objetos de tipo persona
 
-function Persona(nombre, apellido, email){
+function persona(nombre, apellido, email){
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre + ' ' + this.apellido;
+    }
 }
 
-let padre = new Persona('Nemecio','Carrillo','carrillo618@yahoo.com');
+let padre = new persona('Nemecio','Carrillo','carrillo618@yahoo.com');
 console.log(padre);
 
-let madre = new Persona('Carmen','Cegarra','carmencega@hotmail.com');
+let madre = new persona('Carmen','Cegarra','carmencega@hotmail.com');
 console.log(madre);
+
+// modificando algo puntual
+padre.email = 'carrillo852@hotmail.com';
+console.log(padre);
+
+//mostrando el nombre completo
+console.log(padre.nombreCompleto());
+console.log(madre.nombreCompleto());
+
