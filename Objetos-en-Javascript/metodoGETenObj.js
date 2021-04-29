7359e16597a8e0868454ba24512a7c0c9ca5a891
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-04-29 01:55:23 -0400

    metodo SET en JS para modificar un valor de una propiedad

--- Objetos-en-Javascript/metodoGETenObj.js
+++ Objetos-en-Javascript/metodoGETenObj.js
@@ -6,9 +6,24 @@ let persona = {
     nombre: 'Maricarmen',
     apellido: 'Carrillo',
     edad: 23,
+    idioma: 'es',
+    get lang(){
+        // recuperando el valor y lo convertimos en mayúsculas
+        return this.idioma.toLocaleUpperCase();
+    },
+    set lang(lang){
+        // modificando la información de la propiedad
+        this.idioma = lang.toLocaleUpperCase();
+    },
     get nombreCompleto(){
         return this.nombre + ' ' + this.apellido;
     }
 }
 
 console.log(persona.nombreCompleto);
+
+console.log(persona.lang);
+
+persona.lang = 'us';
+
+console.log(persona.lang);
