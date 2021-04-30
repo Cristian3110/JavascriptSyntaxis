241302faad7f7d4bc135f7f440316e5b80349a90
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-04-30 01:28:15 -0400

    Distintas formas de crear objetos en JS

--- Objetos-en-Javascript/metodoConstructor.js
+++ Objetos-en-Javascript/metodoConstructor.js
@@ -27,3 +27,27 @@ console.log(padre);
 console.log(padre.nombreCompleto());
 console.log(madre.nombreCompleto());
 
+/***************************************************
+ * Distintas formas de crear Objetos en Javascript
+ * ***********************************************/
+
+let miObjeto = new Object ();
+let miObjeto2 = new Object ();
+
+let miCadena1 = new String('Hola');
+let miCadena2 = 'Hola';
+
+let miNumero = new Number(1);
+let miNumero2 = 1;
+
+let miBoolean = new Boolean(false);
+let miBoolean2 = false;
+
+let miArreglo = new Array();
+let miArreglo2 = [];
+
+let miFuncion = new Function();
+let miFuncion2 = function(){};
+
+
+
