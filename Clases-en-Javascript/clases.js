228875c6d4bc743eb218da77ddb64c83b38f2493
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-04-30 17:37:01 -0400

    métodos GET y SET en clases de Javascript

--- Clases-en-Javascript/clases.js
+++ Clases-en-Javascript/clases.js
@@ -2,12 +2,23 @@
  * Clases en Javascript
  ************************/
 // Las clases se identifican con mayúsculas al principio
-class Persona{
 
+class Persona{
+    
     constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
     };
+    
+    // colocando los métodos GET y SET 
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
 }
 
 // Dos objetos creados a partir de la misma clase
@@ -17,4 +28,13 @@ console.log(persona1);
 
 let persona2 = new Persona('Mariana','Castillo');
 console.log(persona2);
+
+// mostrando el resultado del método GET
+let persona3 = new Persona('Oscar','Dominguez');
+console.log(persona3.nombre);
+ 
+// mostrando el resultado de la modificación del método SET
+persona3.nombre = 'Alvaro'; // SET nombre
+console.log(persona3.nombre); // GET nombre
+
 
