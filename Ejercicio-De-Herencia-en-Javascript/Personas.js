4c8fdda20d583da6da4f2f37494c5ff9389933e1
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-05-09 23:02:38 -0400

    Ejercicio de Herencia creando la clase Clientes y Junte de clases

--- Ejercicio-De-Herencia-en-Javascript/Personas.js
+++ Ejercicio-De-Herencia-en-Javascript/Personas.js
@@ -10,7 +10,7 @@ class Personas {
     constructor(nombre, apellido, edad){
 
 
-        this._idPersona = ++contadorPersonas;
+        this._idPersona = ++Personas.contadorPersonas;
         this._nombre = nombre;
         this._apellido = apellido;
         this._edad = edad;
@@ -47,6 +47,72 @@ class Personas {
     
     toString(){
         return `${this._idPersona} ${this._nombre} ${this._apellido} ${this._edad}`;
+    }
+
+}
+
+/******************************************
+ * Ejercicio de herencia clase de empleados
+ *******************************************/
+
+ class Empleados extends Personas{
+
+    static contadorEmpleados = 0;
+
+    constructor(sueldo){
+        
+        
+        this._idEmpleado = ++Empleados.contadorEmpleados;
+        this._sueldo = sueldo;
+    }
+
+    get sueldo(){
+        return this._sueldo;
+    }
+
+    set sueldo(sueldo){
+        return this._sueldo = sueldo;
+    }
+
+    toString(){
+        return `${super.toString()} ${this._idEmpleado} ${this._sueldo}`;
+    }
+
+}
+
+
+/****************************************
+ * Ejercicio de Herencia clase Clientes
+ *****************************************/
+
+ class Clientes extends Personas{
+
+    static contadorClientes = 0;
+    
+    constructor(fechaRegistro){  
+
+        let fechaIngreso = new Date();
+
+        this._idCliente = ++Clientes.contadorClientes;
+        this._fechaRegitro = fechaRegistro;
+
+    }
+
+    get idCliente(){
+        return this._idCliente;
+    }
+
+    get fechaRegistro(){
+
+        return this._fechaRegistro;
+    }
+
+    set fechaRegistro(fechaRegistro){
+        return this._fechaRegitro =fechaRegistro;
+    }
+
+    toString(){
+        return `${super.toString()} ${this._idCliente} ${this._fecha}`;
     }
 
 }
