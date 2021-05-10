0b9b3f1a1dbffb1d4ccd504ca15d3534b1715ec0
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-05-09 22:28:12 -0400

    Realizando el ejercicio de Herencia Parte 1 clase de Personas

---
+++ Ejercicio-De-Herencia-en-Javascript/Personas.js
@@ -0,0 +1,55 @@
+/*************************************************
+ * Ejercicio de Herencia, creando clase Personas
+ *********************************************/
+
+
+class Personas {
+
+    static contadorPersonas = 0;
+
+    constructor(nombre, apellido, edad){
+
+
+        this._idPersonas = ++contadorPersonas;
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._edad = edad;
+
+    }
+
+    get idPersona(){
+        return this._idPersonas;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        return this._nombre = nombre;
+    }
+
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+
+    get edad(){
+        return this._edad;
+    }
+
+    set edad(edad){
+        return this._edad = edad;
+    }
+    
+    toString(){
+        return `${this._idPersonas} ${this._nombre} ${this._apellido} ${this._edad}`;
+    }
+
+}
+
+
+
