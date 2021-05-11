d6c4f1ea30271fb1f6f9fb1c2fa06dcd6eb3cdef
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-05-11 01:16:05 -0400

    Creación de clase Orden Parte 2

--- Proyecto Sistema de Ventas/sistemadeVentas.js
+++ Proyecto Sistema de Ventas/sistemadeVentas.js
@@ -52,12 +52,39 @@ class Orden {
 
         this._idOrden = ++Orden.contadorOrdenes;
         this._productos = [];
-        
-        
+        this._contadorProductosAgregados = 0;
     }
 
+    get idOrden(){
+        return this._idOrden;
+    }
 
+    agregarProductos(producto){
+        if(this._productos.length < Orden.MAX_PRODUCTOS){
+            this._productos.push(producto);
+            //this.producto[this._contadorProductosAgregados++] = producto;
+        } else {
+            console.log('No se pueden agregar más productos')
+        }
+    }
 
+    calcularTotal(){
+        
+        let totalVenta = 0;
+        for(let producto of this._productos){
+            totalVenta += producto.precio; // totalVenta = totalVenta + producto.precio
+        }
+        return totalVenta;
+    }
+
+    mostrarOrden(){
+        let productosOrden = '';
+        for(let producto of this._productos){
+            productosOrden += producto.toString() + ' ';
+        }
+
+        console.log(`Orden: ${this._idOrden} Total: ${this.calcularTotal()}, Productos: ${productosOrden}`)
+    }
 
 }
 
