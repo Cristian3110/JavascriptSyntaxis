d64784b1403a4a1ed1398ffe4d5418d98ec5ef10
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-05-19 21:32:27 -0400

    Palabra instanceof en Javascript

--- POO/sobreEscritura.js
+++ POO/sobreEscritura.js
@@ -37,6 +37,24 @@ class Gerente extends Empleado{
 function imprimir(tipo){
     // Depende del tipo va a imprimri el detalle de la clase padre o hija
     console.log(tipo.obtenerDetalles());
+    // Aplicando la palabra instanceof
+    /**
+     * Nota: Para utilizar instanceof, y queremos determinar el tipo de una clase, colocar a clase de menor
+     * jerarquía y posterior las de mayor jerarquía
+     */
+
+    if(tipo instanceof Gerente){
+        console.log('Es un objeto de tipo Gerente');
+        // Esto nos sirve para saber que estamos en la instancia de Gerente y podemos solicitar el departamento
+        console.log(tipo.departamento);
+        console.log(tipo.empleado1);
+    }
+    else if(tipo instanceof Empleado){
+        console.log('Es un objeto de tipo Empleado');
+    }
+    else if(tipo instanceof Object){
+        console.log('Es un objeto de tipo Object');
+    }
 }
 
 let empleado1 = new Empleado('Nemecio', 3000);
