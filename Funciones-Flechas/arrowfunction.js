d6d3faf23048cef2654048b1a052f3196c90e1d4
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-05-21 02:50:39 -0400

    funciones flechas o arrow function part 2

--- Funciones-Flechas/arrowfunction.js
+++ Funciones-Flechas/arrowfunction.js
@@ -29,6 +29,37 @@ const adicion = (a,b)=>{
 }
 console.log(`El resultado de la suma es: ${adicion(1,9)}`);
 
+//Función flecha 
+const funcionFecha2 = (c,d) => console.log(`El resultado de la resta es: ${c-d}`);
+funcionFecha2(5,3);
+
+const saludar = ()=>{
+    return  'Saludos desde saludar';
+}
+console.log(saludar());
+
+// Para simplificar la estructura de una función flecha puede ser así
+const saluddar2 = () => 'Saludos desde saludar2';
+console.log(saluddar2());
+
+const regresarObjeto = ()=>({nombre: 'Cristian', apellido:'Carrillo'});
+console.log(regresarObjeto());
+
+//Funciones con parámetros
+const funcionConParametros = (mensaje)=> console.log(`Tu mensaje introducido fue: ${mensaje}`);
+funcionConParametros('Saludos desde la Función con Parámetros');
+
+const funcionParametros1 = (msj)=> `EL mensaje que introdujiste fue: ${msj}`;
+console.log(funcionParametros1('Hola'));
+
+const salida = mensaje => `EL mensaje de salida es: ${mensaje}`;
+console.log(salida('EXIT'));
+
+const multiplicacion = (num1,num2)=>{
+    let resultado = num1 * num2;
+    return resultado;
+}
+console.log(multiplicacion(5,6));
 
 
 
