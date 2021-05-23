fd4d5f2362c5f7f0940557a9b4f253af8d22913b
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-05-23 12:46:18 -0400

    Palabra Async con Promesas en Javascript

---
+++ Promesas en Javascript/async.js
@@ -0,0 +1,18 @@
+/*****************************************************************
+ * Palabras async 
+ * Ésta palabra lo que indica es que la función va a regresar una promesa
+ *******************************************************************/
+
+ let promesa = new Promise((resolve=>{
+    
+    setTimeout(()=> resolve('Saludos desde Promesa1 y timeout a los 3seg'),3000);
+    
+}));
+
+
+async function miFuncionConPromesa(){
+    return ('Saludos con Promesa y Async');
+}
+
+miFuncionConPromesa().then((valor)=>console.log(valor));
+
