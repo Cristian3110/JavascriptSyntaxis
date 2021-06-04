1f24022d059a1f329d51dfa8c31e76e3f37a6190
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-06-04 02:04:46 -0400

    accediendo al DOM HTML con Javascript

--- Manejo del DOM HTML con JS/script.js
+++ Manejo del DOM HTML con JS/script.js
@@ -1,1 +1,12 @@
-console.log('Saludos desde JAvascript');
+/*********************************************************************************************
+ * En caso de que queramos colocar el script de JS, se debe colocar después de las etiquetas de HTML
+ * para que se puedan evaluar con el script de Javascript
+ *************************************************************/
+
+console.log('Saludos desde Javascript');
+
+let cabecero = document.getElementById('cabecero').innerHTML;
+console.log(`El valor del cabecero es: ${cabecero}`);
+
+let parrafo = document.getElementById('parrafo').innerHTML;
+console.log(`El valor del parrafo es: ${parrafo}`);
