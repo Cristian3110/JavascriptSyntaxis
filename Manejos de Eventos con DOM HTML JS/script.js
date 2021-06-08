c773caae00e3b47084ddd557b304738c0553cec6
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-06-08 19:35:57 -0400

    Asociar eventos a Elementos HTML con Javascript

--- Manejos de Eventos con DOM HTML JS/script.js
+++ Manejos de Eventos con DOM HTML JS/script.js
@@ -1,4 +1,4 @@
-/********************************************************************   
+/********************************************************************
  * Realizando la función que ejecutará el método para cambiar el texto
  ********************************************************************/
 
@@ -9,3 +9,21 @@ function cambiarTexto(titulo) {
   console.log(titulo.innerHTML);
 }
 
+// Asociar eventos a elementos HTML DOM con JS
+// Sólo pasando la referencia a esta función, no necesita paréntesis
+document.getElementById("titulo1").onclick = cambiandoTexto;
+
+function cambiandoTexto() {
+  document.getElementById("titulo1").innerHTML = "El nuevo cambio de título";
+}
+
+// Ahora realizando lo mismo para agregar nuevo contenido
+
+document.getElementById("nuevoContenido").onclick = nuevoContenido;
+
+function nuevoContenido() {
+  document.getElementById("mostrarContenido").innerHTML =
+    "Nuevo Contenido de la Página";
+}
+
+// éstas son funciones de estilo CallBack
