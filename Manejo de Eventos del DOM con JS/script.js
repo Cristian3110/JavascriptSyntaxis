5a266950341f5d7c956d2ee2d3f04f3002076a09
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-06-09 12:15:46 -0400

    Funciones feclas o arrow function en addEventListener con Javascript

--- Manejo de Eventos del DOM con JS/script.js
+++ Manejo de Eventos del DOM con JS/script.js
@@ -1,54 +1,71 @@
 // Definiendo la función
 
-function convertir(){
-    let nombre = document.getElementById('nombre');
-    nombre.value = nombre.value.toUpperCase();
+function convertir() {
+  let nombre = document.getElementById("nombre");
+  nombre.value = nombre.value.toUpperCase();
 }
 
-function convertir2(apellidoInput){
-    apellidoInput.value = apellidoInput.value.toUpperCase();
+function convertir2(apellidoInput) {
+  apellidoInput.value = apellidoInput.value.toUpperCase();
 }
 
-function purpura(subtitulo){
-    subtitulo.style.color = '#560bad';
+function purpura(subtitulo) {
+  subtitulo.style.color = "#560bad";
 }
-function blue(subtitulo){
-    subtitulo.style.color = "#4895ef"
-}
-
-function azulO(subtitulo){
-    subtitulo.style.color = '#03045e';
-}
-function naranjaO(subtitulo){
-    subtitulo.style.color = "#e85d04"
+function blue(subtitulo) {
+  subtitulo.style.color = "#4895ef";
 }
 
-function verde(titulo){
-    titulo.style.color = '#14746f'
+function azulO(subtitulo) {
+  subtitulo.style.color = "#03045e";
+}
+function naranjaO(subtitulo) {
+  subtitulo.style.color = "#e85d04";
 }
 
-function cambiar(elementoInput){
-    elementoInput.style.background = "#023e8a";
+function verde(titulo) {
+  titulo.style.color = "#14746f";
 }
 
-function regresar(elementoInput){
-    elementoInput.style.background = "white";
+function cambiar(elementoInput) {
+  elementoInput.style.background = "#023e8a";
+}
+
+function regresar(elementoInput) {
+  elementoInput.style.background = "white";
 }
 
 // Cambio en los elementos email
-document.getElementById('email').addEventListener('focus',cambiar);
-document.getElementById('email').addEventListener('blur',devolver);
+document.getElementById("email").addEventListener("focus", cambiar);
+document.getElementById("email").addEventListener("blur", devolver);
 
 //cambios en el elemento pais
-document.getElementById('pais').addEventListener('focus',cambiar);
-document.getElementById('pais').addEventListener('blur',devolver);
+document.getElementById("pais").addEventListener("focus", cambiar);
+document.getElementById("pais").addEventListener("blur", devolver);
 
-function cambiar(evento){
-    let componente = evento.target;
-    componente.style.background = '#023e8a';
+function cambiar(evento) {
+  let componente = evento.target;
+  componente.style.background = "#023e8a";
 }
 
-function devolver(evento){
-    let componente = evento.target;
-    componente.style.background = 'white';
+function devolver(evento) {
+  let componente = evento.target;
+  componente.style.background = "white";
 }
+
+// Realizando la misma operación pero implementando función flecha o arrow function
+// La desventaja es que ésta función no se puede reutilizar para otras etiquetas por ejemplo
+
+document
+  .getElementById("estado")
+  .addEventListener(
+    "focus",
+    (evento) => (evento.target.style.background = "red")
+  );
+
+document
+  .getElementById("estado")
+  .addEventListener(
+    "blur",
+    (evento) => (evento.target.style.background = "white")
+  );
