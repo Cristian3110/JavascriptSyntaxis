a286fcac563f07cfe7df2f7d8d9cf6fe1b8fda37
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-06-08 23:55:13 -0400

    Manejo del Evento Onchange en Javascript

---
+++ Manejo de Eventos del DOM con JS/script.js
@@ -0,0 +1,10 @@
+// Definiendo la función
+
+function convertir(){
+    let nombre = document.getElementById('nombre');
+    nombre.value = nombre.value.toUpperCase();
+}
+
+function convertir2(apellidoInput){
+    apellidoInput.value = apellidoInput.value.toUpperCase();
+}
