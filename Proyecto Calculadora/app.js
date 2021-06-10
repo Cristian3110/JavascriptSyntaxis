b5f260545ea40d4d0cf8e643f86ba6a3eaf5afe6
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-06-09 23:32:24 -0400

    configurando las demás operaciones y sus botones

--- Proyecto Calculadora/app.js
+++ Proyecto Calculadora/app.js
@@ -11,5 +11,47 @@ function sumar() {
   if(isNaN(resultado)) {
     resultado = "La operación no incluye números";
   }
+  console.log(resultado);
   document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
 }
+
+function restar(){
+    const forma = document.getElementById("forma");
+  let operandoA = forma["operandoA"];
+  let operandoB = forma["operandoB"];
+  // Conviertiendo los valores a enteros con ParseInt
+  let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+  if(isNaN(resultado)) {
+    resultado = "La operación no incluye números";
+  }
+  console.log(resultado);
+  document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
+}
+
+function multiplicar(){
+    const forma = document.getElementById("forma");
+  let operandoA = forma["operandoA"];
+  let operandoB = forma["operandoB"];
+  // Conviertiendo los valores a enteros con ParseInt
+  let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+  if(isNaN(resultado)) {
+    resultado = "La operación no incluye números";
+  }
+  console.log(resultado);
+  document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
+}
+
+
+function dividir(){
+    const forma = document.getElementById("forma");
+  let operandoA = forma["operandoA"];
+  let operandoB = forma["operandoB"];
+  // Conviertiendo los valores a enteros con ParseInt
+  let resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+  if(isNaN(resultado)) {
+    resultado = "La operación no incluye números";
+  }
+  console.log(resultado);
+  document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
+}
+
