ab1dc3f792ff7241d33dcb0eb1bfd1634b7aae53
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-06-10 19:41:50 -0400

    Formulario agregar nueva persona & lógica a nuestra App con Javascript

--- aplicación de personas/js/app.js
+++ aplicación de personas/js/app.js
@@ -13,3 +13,17 @@ function mostrarPersonas() {
   }
   document.getElementById("personas").innerHTML = texto;
 }
+
+function agregarPersona() {
+  const forma = document.forms["forma"];
+  const nombre = forma["nombre"];
+  const apellido = forma["apellido"];
+  if ((nombre.value == "") || (apellido.value == "")) {
+    alert("Por favor introduzca un nombre y un apellido");
+  } else {
+    const persona = new Persona(nombre.value, apellido.value);
+    console.log(persona);
+    personas.push(persona);
+    mostrarPersonas();
+  }
+}
