cf8fb199a411c6454e9304bc4b1b85e0658ec2a9
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-06-10 19:12:27 -0400

    Listado de Personas con Javascript Fase2

--- aplicación de personas/js/app.js
+++ aplicación de personas/js/app.js
@@ -1,1 +1,15 @@
-console.log('listado de personas');
+const personas = [
+  new Persona("Nemecio", "Carrillo"),
+  new Persona("Carmen", "Cegarra"),
+  new Persona("Maricarmen", "Carrillo"),
+];
+
+function mostrarPersonas() {
+  console.log("Ejecutando el método mostrar persona");
+  let texto = "";
+  for (let persona of personas) {
+    console.log(persona);
+    texto += `<li>${persona._nombre} ${persona._apellido}</li>`;
+  }
+  document.getElementById("personas").innerHTML = texto;
+}
