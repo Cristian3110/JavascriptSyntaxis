5a54c029148598d80465dda7ac60377e9c240e24
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2021-06-10 21:17:19 -0400

    configuración lógica de la aplicación con el código Jasvascritp

--- Reloj Digital con Js/js/reloj.js
+++ Reloj Digital con Js/js/reloj.js
@@ -1,1 +1,48 @@
-console.log('conectado');
+const mostrarReloj = () => {
+  let fecha = new Date();
+  let hr = formatoHora(fecha.getHours());
+  let min = formatoHora(fecha.getMinutes());
+  let seg = formatoHora(fecha.getSeconds());
+
+  document.getElementById("hora").innerHTML = `${hr}: ${min}: ${seg}`;
+
+  const meses = [
+    "enero",
+    "febrero",
+    "marzo",
+    "abril",
+    "mayo",
+    "junio",
+    "julio",
+    "agosto",
+    "septiembre",
+    "octubre",
+    "noviembre",
+    "diciembre",
+  ];
+  const dias = [
+    "domingo",
+    "lunes",
+    "martes",
+    "miercoles",
+    "jueves",
+    "viernes",
+    "sabado",
+  ];
+
+  let diaSemana = dias[fecha.getDay()];
+  let dia = fecha.getDate();
+  let mes = meses[fecha.getMonth()];
+  let fechaTexto = `${diaSemana}, ${dia} ${mes}`;
+
+  document.getElementById("fecha").innerHTML = fechaTexto;
+};
+
+// configurando los digitos en el formato
+const formatoHora = (hora) => {
+  if (hora < 10) hora = "0" + hora;
+  return hora;
+};
+
+// configurando el tiempo de carga del reloj
+setInterval(mostrarReloj, 1000);
